6c793e0cd707777d213f0f060e0ad9fb2d77e258
Yael Demers <35872660+yaeldemers@users.noreply.github.com>
2021-07-22 16:47:10 -0400

    Uploading the first DOM project
    Here, I made a BMI calculator using a bit of CSS for styling

---
+++ DOM_projects/BMI_calculator/app.js
@@ -0,0 +1,35 @@
+const calculateBtn = document.querySelector("#calculate_bmi");
+const bmiResults = document.querySelector("#bmi_result");
+const form = document.querySelector("form");
+
+calculateBtn.addEventListener("click", (event) => {
+    const weight = document.querySelector("#weightField").value;
+    const height = document.querySelector("#heightField").value;
+
+    if (weight === "" || height ==="" || weight<=0 || height<=0) {
+        return alert("Error, please enter proper height and weight.");
+    }
+
+    const result = weight / (height * height);
+
+    let bmiMsg;
+    let msgColor = "text-danger";
+
+    if(result<18.5) {
+        bmiMsg = "Thinness";
+    } else if(result>=18.5 && result <=25) {
+        bmiMsg = "Normal";
+        msgColor = "text-success";
+    } else if(result>25 && result <30) {
+        bmiMsg = "Overweight";
+    } else if(result>=30) {
+        bmiMsg = "Obese";
+    }
+
+    bmiResults.innerHTML = `<p id="bmi_result">BMI = <b>${Math.round(result*100)/100}</b> (<span 
+        class="${msgColor}"><b>${bmiMsg}</b></span>)</p>`
+
+} );
+
+//prevent page from loading
+form.addEventListener("submit", e => {e.preventDefault();})
